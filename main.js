fdca416cacc609b093f4fd12cfc013a681f76351
liwag-9e-ay2526 <liwag.gc@obmontessori.edu.ph>
2026-04-24 10:11:00 +0800

    Add animalCount function to alert zoo animals

---
+++ main.js
@@ -0,0 +1,6 @@
+function animalCount() {
+    var animals = ["Lizard", "Capybara", "Bear", "Beaver"];
+        for (var i = 0; i < animals.length; i++) {
+            window.alert ("This zoo contains a " + animals[i] + "." );
+        } 
+}
